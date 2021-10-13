a5097dc006c63dadcb52736b39d5c21a7ad29b6f
Yago Ananias <yago_souza92@yahoo.com.br>
2021-10-13 20:40:36 -0300

    Finished

--- Semana10/classe.c
+++ Semana10/classe.c
@@ -6,7 +6,16 @@ struct Node{
   struct Node *next; //ponteiro para outro Node
 };
 
+//imprimir registro de uma lista ligada
+void printList(struct Node *node) {
+  while(node != NULL) {
+    printf("%d\n", node->data);
+    node = node->next;
+  }
+}
+
 int main(void) {
+  //ponteiros do tipo struct Node
   struct Node *head = NULL;
   struct Node *second;
   struct Node *third;
@@ -15,4 +24,19 @@ int main(void) {
   head = (struct Node*)malloc(sizeof(struct Node));
   second = (struct Node*)malloc(sizeof(struct Node));
   third = (struct Node*)malloc(sizeof(struct Node));
+
+  //populando os registros e criando a lista
+  //começando com a cabeça da lista
+  head->data = 1;
+  head->next = second;
+  
+  second->data = 2;
+  second->next = third;
+  
+  third->data = 3;
+  third->next = NULL;
+
+  printList(head);
+
+  return 0;
 }
